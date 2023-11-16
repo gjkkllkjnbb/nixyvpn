f809d3c3f84a1c08f37096a874398ac1947d7a86
Nixxx NZ <142969532+Nixxx-XD@users.noreply.github.com>
2023-11-17 02:47:28 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,211 @@
+<p align="center">
+<img src="https://readme-typing-svg.herokuapp.com?color=%2336BCF7&center=true&vCenter=true&lines=S++C++R++I++P+T++~++M++O++D++~++B++Y" />
+<img src="https://readme-typing-svg.herokuapp.com?color=%2336BCF7&center=true&vCenter=true&lines=T++A++R++A++P+~+K++U++H++I++N++G" />
+</p>
+<p align='center'><a href="https://api.daily.dev/get?r=Tarap-Kuhing"><img src="https://user-images.githubusercontent.com/120649258/210357165-62adb36d-600b-449e-bccd-d491ac5bb24b.gif?r=82s" width="150" alt="Hayuk"/></a></p>
+
+<h2 align="center">
+Auto Script Install VPN Service
+Mod By TARAP KUHING
+Wa : 085754292950
+
+<img src="https://img.shields.io/badge/Version-1.0.0-blue.svg"></h2>
+
+</p>
+<h2 align="center"> Supported Linux Distribution</h2>
+<p align="center"><img src="https://d33wubrfki0l68.cloudfront.net/5911c43be3b1da526ed609e9c55783d9d0f6b066/9858b/assets/img/debian-ubuntu-hover.png"></p>
+<p align="center"><img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%209&message=Stretch&color=purple"> <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%2010&message=Buster&color=purple">  <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=ubuntu&label=Ubuntu%2018&message=Lts&color=red"> <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=ubuntu&label=Ubuntu%2020&message=Lts&color=red">
+</p>
+
+<p align="center"><img src="https://img.shields.io/badge/Service-SSH_Websocket-success.svg">  <img src="https://img.shields.io/badge/Service-OpenVPN_Websocket-success.svg">  <img src="https://img.shields.io/badge/Service-SSLH-success.svg">  <img src="https://img.shields.io/badge/Service-Stunnel5-success.svg">  <img src= "https://img.shields.io/badge/Service-OHP_Open_Http_Puncher-success.svg">  <img src= "https://img.shields.io/badge/Service-SSTP_VPN-success.svg">  <img src= "https://img.shields.io/badge/Service-L2TP_VPN-success.svg">  <img src= "https://img.shields.io/badge/Service-PPTP_VPN-success.svg">
+<p align="center"><img src="https://img.shields.io/badge/Service-SSH_OpenSSH-success.svg">  <img src="https://img.shields.io/badge/Service-SSH_Dropbear-success.svg">  <img src="https://img.shields.io/badge/Service-BadVPN-success.svg">  <img src="https://img.shields.io/badge/Service-OpenVPN-success.svg">  <img src="https://img.shields.io/badge/Service-Squid3-success.svg">  <img   src="https://img.shields.io/badge/Service-Webmin-success.svg">  <img src="https://img.shields.io/badge/Service-SlowDns-success.svg">  <p align="center"><img src="https://img.shields.io/badge/Service-XRAY-success.svg">  <img src="https://img.shields.io/badge/Service-XRAY_Websocket_TLS-success.svg">  <img src="https://img.shields.io/badge/Service-XRAY_VLESS_VMESS-success.svg">  <img src="https://img.shields.io/badge/Service-XRAY_gRPC_VLESS_VMESS-success.svg">  <img src="https://img.shields.io/badge/Service-XRAY_TROJAN-success.svg">  <p align="center"><img src="https://img.shields.io/badge/Service-SSR-success.svg">  <img src="https://img.shields.io/badge/Service-Trojan_Go-success.svg">  <img src="https://img.shields.io/badge/Service-WireGuard-success.svg">  <img src= "https://img.shields.io/badge/Service-Shadowsocks-success.svg">  
+
+### Info By Tarap Kuhing:
+Buat VPS Fresh atau VPS Baru dan Wajib Menggunakan User: root
+* Login VPS User menggunakan user root
+* Cara langsung masuk ke akses root
+```html
+sudo su
+```
+Atau
+```html
+sudo -i
+```
+atau
+```html
+su
+```
+
+### Link FB Saya
+```html
+https://m.facebook.com/100050453861530/posts/628374871570224/?app=fbl
+```
+
+## Installation Script By Tarap Kuhing
+## 1.
+Part 1: Update dan Upgrade
+   <img src="https://img.shields.io/badge/Update%20Upgrade-green">
+  ```html
+apt-get update && apt-get upgrade -y && reboot
+```
+
+## 2.0
+Aktifkan Root Sementara
+  ```html
+sudo su
+cd
+cd
+
+```
+
+# 2.
+Part 2: Buat Akses Root Di VPS /Root VPS
+  <img src="https://img.shields.io/badge/Akses_Root%20VPS-green">
+Untuk Mengizinkan Root Dan Ubah Password Login di VPS Google Cloud Platform, Aws, Dan Lain-lain
+   ```html
+  wget -qO- -O vpsroot.sh https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/vpsroot.sh && bash vpsroot.sh
+  ```
+
+# 2.5
+Di sini wajib pake user root atau bisa aktifkan root sementara
+   ```html
+sudo su
+cd
+cd
+
+  ```
+### 3.
+Part 3: Install Layanan VPN Service
+Mod By KANGSANTRI 
+  <img src="https://img.shields.io/badge/Install_Semua_Layanan_VPN%20Batch-green">
+  ```html
+rm -f setup.sh && sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/Kangsantri123/scriptvps/main/setup.sh && chmod +x setup.sh && ./setup.sh 
+
+```
+
+### 4. DONE / SELESAI
+* • jika tidak bisa login di vps ,gunakan port ssh
+* • 22, 2253
+
+### Fitur Script Mod By TARAP KUHING
+
+• SSH & OpenVPN
+
+• SSH Websocket TLS & No TLS
+
+• OHP SSH & OHP Dropbear & OHP OpenVPN
+
+• XRAY VMESS
+
+• XRAY VLESS
+
+• XRAY TROJAN
+
+• SHADOWSOCKS
+
+• SSR
+
+• PPTP VPN
+
+• L2TP VPN
+
+• SSTP VPN
+
+• WIREGUARD
+
+• TROJAN GO
+
+• Backup Data ALL Service
+
+• Restore Data ALL Service
+
+### Os Supported
+
+• Debian 10 Only
+
+• Ubuntu 18.04 & 20.04 (Recommended)
+
+# Service & Port
+
+• OpenSSH                 : 443, 22
+
+• OpenVPN                 : TCP 1194, UDP 2200, SSL 990
+
+• Stunnel5                : 443, 445, 777
+
+• Dropbear                : 443, 109, 143
+
+• Squid Proxy             : 3128, 8080
+
+• Badvpn                  : 7100, 7200, 7300
+
+• Nginx                   : 89
+
+• Wireguard               : 7070
+
+• L2TP/IPSEC VPN          : 1701
+
+• PPTP VPN                : 1732
+
+• SSTP VPN                : 444
+
+• Shadowsocks-R           : 1443-1543
+
+• SS-OBFS TLS             : 2443-2543
+
+• SS-OBFS HTTP            : 3443-3543
+
+• XRAYS Vmess TLS         : 8443
+
+• XRAYS Vmess None TLS    : 80
+
+• XRAYS Vless TLS         : 8443
+
+• XRAYS Vless None TLS    : 80
+
+• XRAYS Trojan            : 2083
+
+• Websocket TLS           : 443
+
+• Websocket None TLS      : 8880
+
+• Websocket Ovpn          : 2086
+
+• OHP SSH                 : 8181
+
+• OHP Dropbear            : 8282
+
+• OHP OpenVPN             : 8383
+
+• Trojan Go               : 2087
+
+ ### Server Information & Other Features
+
+• Timezone                : Asia/Jakarta (GMT +7)
+
+• Fail2Ban                : [ON]
+
+• Dflate                  : [ON]
+
+• IPtables                : [ON]
+
+• Auto-Reboot             : [ON]
+
+• IPv6                    : [OFF]
+
+• Autoreboot On 05.00 GMT +7
+
+• Futo Delete Expired Account
+
+• Full Orders For Various Services
+
+• White Label
+
+
+
+
+
+=======================
+**Mod By Tarap Kuhing**
+**WA ME :085754292950**
+=======================
---
+++ setup.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl "https://raw.githubusercontent.com/Tarap-Kuhing/perizinan/main/ipvps.txt" | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : "
+echo -e "${NC}${LIGHT}WhatsApp : 085754292950"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/Hendra2012"
+exit 0
+fi
+# ==========================================
+# Getting
+# Link Hosting Kalian Untuk Ssh Vpn
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh"
+# Link Hosting Kalian Untuk Sstp
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/sstp"
+# Link Hosting Kalian Untuk Ssr
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssr"
+# Link Hosting Kalian Untuk Shadowsocks
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/shadowsocks"
+# Link Hosting Kalian Untuk Wireguard
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/wireguard"
+# Link Hosting Kalian Untuk Xray
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray"
+# Link Hosting Kalian Untuk Ipsec
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ipsec"
+# Link Hosting Kalian Untuk Backup
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/backup"
+# Link Hosting Kalian Untuk Websocket
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/websocket"
+# Link Hosting Kalian Untuk Ohp
+tarapkuhing="raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ohp"
+
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$(wget -qO- ipinfo.io/ip);
+
+rm -f setup.sh
+clear
+if [ -f "/etc/xray/domain" ]; then
+echo "Script Already Installed"
+exit 0
+fi
+mkdir /var/lib/tarapkuhing;
+echo "IP=" >> /var/lib/tarapkuhing/ipvps.conf
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/cf.sh && chmod +x cf.sh && ./cf.sh
+#install v2ray
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/xray/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
+#install ssh ovpn
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/sstp/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+#install ssr
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssr/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/shadowsocks/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+#installwg
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/wireguard/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+#install L2TP
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ipsec/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+# Websocket
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/websocket/edu.sh && chmod +x edu.sh && ./edu.sh
+# Ohp Server
+wget https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ohp/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+
+rm -f /root/ssh-vpn.sh
+rm -f /root/sstp.sh
+rm -f /root/wg.sh
+rm -f /root/ss.sh
+rm -f /root/ssr.sh
+rm -f /root/ins-xray.sh
+rm -f /root/ipsec.sh
+rm -f /root/set-br.sh
+rm -f /root/edu.sh
+rm -f /root/ohp.sh
+cat <<EOF> /etc/systemd/system/autosett.service
+[Unit]
+Description=autosetting
+Documentation=https://t.me/Hendra2012
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /etc/set.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable autosett
+wget -O /etc/set.sh "https://raw.githubusercontent.com/Tarap-Kuhing/scriptvps/main/ssh/set.sh"
+chmod +x /etc/set.sh
+history -c
+echo "1.2" > /home/ver
+echo " "
+echo "Installation has been completed!!"echo " "
+echo "============================================================================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "----------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 443, 22"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 990"  | tee -a log-install.txt
+echo "   - Stunnel5                : 443, 445, 777"  | tee -a log-install.txt
+echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+echo "   - Wireguard               : 7070"  | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
+echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
+echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
+echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
+echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
+echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
+echo "   - XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
+echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
+echo "   - XRAYS Vless TLS         : 8443"  | tee -a log-install.txt
+echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
+echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
+echo "   - Websocket TLS           : 443"  | tee -a log-install.txt
+echo "   - Websocket None TLS      : 8880"  | tee -a log-install.txt
+echo "   - Websocket Ovpn          : 2086"  | tee -a log-install.txt
+echo "   - OHP SSH                 : 8181"  | tee -a log-install.txt
+echo "   - OHP Dropbear            : 8282"  | tee -a log-install.txt
+echo "   - OHP OpenVPN             : 8383"  | tee -a log-install.txt
+echo "   - Tr Go                   : 2087"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""
+echo "   - Installation Script vps Mod By TARAP KUHING......SELESAI......!!!!!!"
+echo ""
+echo " Reboot 15 Sec"
+sleep 15
+rm -f setup.sh
+reboot
